6257b32313f37ba1a94748d90b3e85d8771ba305
Maxime <maxime.chevalier@iut-valence.fr>
2016-05-26 10:58:18 +0200

    Ajout des prototypes de fonctions

--- src/LE/lectureEcriture.h
+++ src/LE/lectureEcriture.h
@@ -1,7 +1,18 @@
 #ifndef LECTUREECRITURE_H
+	#define LECTUREECRITURE_H
 	#include "sdata.h"
+	
+	extern FILE * fichierACompresser;
+	extern FILE * fichierTemporaire;
+	extern FILE * fichierCompresse;
+	extern FILE * fichierADecompresser;
+	
 	void lireFichier(donnees * d);
 	void realisationCompression(Arbre * arbre);
+	int lectureOctet(FILE * fichier, char* symbole);
+	int lectureBit(FILE * fichier, char* symbole);
+	int ecritureOctet(FILE * fichier, char* symbole);
+	int ecritureBit(FILE * fichier, char* symbole);
 #endif
 
 
