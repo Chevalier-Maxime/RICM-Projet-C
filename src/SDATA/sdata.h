e463758eb4cc279a8b453dbfe294a8624cd458e9
Maxime <maxime.chevalier@iut-valence.fr>
2016-05-26 08:21:00 +0200

    Ajout des fichiers de base

---
+++ src/SDATA/sdata.h
@@ -0,0 +1,33 @@
+#define TAILLE_SEQUENCE_DEBUT //#TODO a definir
+#define SEQUENCE_DEBUT //#TODO a definir
+
+typedef struct donnees
+{
+	Arbre* racineArbreBinaire;
+	int nbSymboles;
+	int Lmax; //donné
+} donnees;
+
+typedef struct Arbre
+{
+	Arbre* filsGauche;
+	Arbre* filsDroit;
+	int valeur;
+} Arbre;
+
+typedef struct entete
+{
+	int nbSymboles;
+	Arbre* arbreCanonique;
+	int nbPadding;
+	int tailleEntete;
+} entete;
+
+union octet{
+	char symbole;
+	//#TODO a voir...
+}
+
+void creerArbreBinaire(int hauteur, Arbre * arbre);
+int rechercheArbreBinaire(Arbre * arbre, char symbole);
+
