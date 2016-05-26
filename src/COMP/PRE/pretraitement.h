2ef0592a67ede53e81b273a8d7dfbbddea9ed143
amaurina <amaurin.alexandre@gmail.com>
2016-05-26 12:04:18 +0200

    precodage RLE

---
+++ src/COMP/PRE/pretraitement.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void pretraiter( FILE* fichier, FILE* fichierPretraite);
+
+void ecriture(int i,FILE* fichierPretraite);
+
+int testerreurCaractere(int courant);
+
+int testfinCaractere(int courant);
+
+int sontIdentique(int courant, int suivant);
+
