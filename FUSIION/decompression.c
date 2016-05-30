8ad464b2ae008ade8e8b9169f1804f7ac27eaa9e
Hugo <amodrufavin.hugo@gmail.com>
2016-05-30 16:18:42 +0200

    Last one ? decomp

--- FUSIION/decompression.c
+++ FUSIION/decompression.c
@@ -6,33 +6,29 @@
 
 ArbreSymbole* Decompression(unsigned char TS[256], int TT[256], int nbSymboles){
 	TabArb Arb;
-	int i, j, Prof;
+	int i, j;
 
 	for (j = 0; j<nbSymboles; j++) {
 		Arb.a[j] = creerArbreSymboleVide(TS[j], TT[j]);
 	}
 	Arb.Taille = nbSymboles;
-	
-	Prof = TT[j];
+
 	while (Arb.Taille>2) {
 
 		Arb.a[0] = ajout2ArbresS(Arb.a[0], Arb.a[1]);
+
 		for (j = 1; j<Arb.Taille; j++)
-			Arb.a[j] = Arb.a[j+1];
+			Arb.a[j] = Arb.a[j + 1];
 		Arb.Taille--;
 
 		i = 1;
-		while (i<Arb.Taille-1 && 2*Prof == Arb.a[i]->occurrence + Arb.a[i+1]->occurrence)) {
-			Arb.a[i] = ajout2ArbresS(Arb.a[i], Arb.a[i+1]);
+		while (i<Arb.Taille-1 && (Arb.a[0]->occurrence >= Arb.a[i]->occurrence + Arb.a[i + 1]->occurrence)) {
+			Arb.a[i] = ajout2ArbresS(Arb.a[i], Arb.a[i + 1]);
 			i++;
-			for (j = i; j<Arb.Taille; j++){
+			for (j = i; j<Arb.Taille; j++)
 				Arb.a[j] = Arb.a[j+1];
-				if(Arb.a[j]->occurrence>Prof)
-					Arb.a[j]->occurrence=Prof;
-			}
 			Arb.Taille--;
 		}
-		Prof--;
 	}
 	if(Arb.Taille>=0){
 		Arb.a[0] = ajout2ArbresS(Arb.a[0], Arb.a[1]);
